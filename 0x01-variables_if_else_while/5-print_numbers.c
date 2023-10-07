25b2bf95e8466779e6bb520cc16eb199e3779e13
oladele-david <tdboy59@gmail.com>
2023-10-07 04:19:31 -0700

    task 5 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Returns the single numbers base 10
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
