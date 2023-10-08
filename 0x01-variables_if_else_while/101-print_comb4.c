cf79136a48d1f19df1691ef3cc88b67c65e10f95
oladele-david <tdboy59@gmail.com>
2023-10-08 08:40:33 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int digit1, digit2, digit3;
+	
 	for (digit1 = 0; digit1 <= 7; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
@@ -27,37 +28,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int digit1, digit2, digit3;
-
-	for (digit1 = 0; digit1 <= 7; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 <= 8; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
-			{
-				putchar('0' + digit1);
-				putchar('0' + digit2);
-				putchar('0' + digit3);
-
-				if (digit1 < 7 || digit2 < 8 || digit3 < 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
-}
 	return (0);
 }
