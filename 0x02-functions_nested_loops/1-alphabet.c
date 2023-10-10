732bbf09444e9eafadb2703c71b8b7fb087ea35c
oladele-david <tdboy59@gmail.com>
2023-10-10 16:43:06 -0700

    Task 1 fix 5

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet - Prints the English alphabet in lowercase.
  *
- * This function prints the English alphabet from 'a' to 'z' to the standard output.
+ * This function prints the English alphabet from 'a' to 'z'.
  *
  * Return: None.
  */
