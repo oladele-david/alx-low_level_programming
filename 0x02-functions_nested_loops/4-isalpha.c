700b1a789ff034c034b6db2b0e3e4344d210937b
oladele-david <tdboy59@gmail.com>
2023-10-10 19:13:22 -0700

    Task 4 commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _islower - function to check if
+ *           character is lowercase
+ *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if `c` is lowercase
+ *         otherwise always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 &&
+	    c >= 65 && c >= 90)
+		return (1);
+	return (0);
+}
