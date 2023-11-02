4ddcc784f82430a9a27698cd82b497820fe18b7d
oladele-david <tdboy59@gmail.com>
2023-11-02 07:52:26 -0700

    Task commit

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks the code for title-case.
+ *
+ * @c: is an integer params
+ *
+ * Return: Always 0.
+ */
+
+
+int _isupper(int c)
+{
+
+return (c >= 65 && c <= 90);
+}
