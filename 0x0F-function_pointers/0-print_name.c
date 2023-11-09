9ff50b696ac53688a1490e4e7f7de1fb05e28e96
oladele-david <tdboy59@gmail.com>
2023-11-09 00:20:05 -0800

    pushing p

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
+#include "function_pointers.h"
+
 /**
- * print_name - function to print name
- *
- * @name: name to print
- * @f: function pointer that doesn't return anything
- *
- * Return: Always 0 (Success)
-*/
+ * print_name - prints a name
+ * @name: string
+ * @f: function
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-		f(name);
+if (f != NULL)
+f(name);
 }
