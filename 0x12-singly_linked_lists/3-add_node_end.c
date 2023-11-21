d41791de9592d7d6d1ff4749b9db6d1286ae5205
oladele-david <tdboy59@gmail.com>
2023-11-21 06:27:47 -0800

    new fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 
 /**
- * add_node_end - Add a new node at the end of a list in program.
- * @head: Address of the first node in a list.
- * @str: Address of the string to insert into the new node created.
- * Return: Address of the new node.
+ *add_node_end - Add a new node at the end of a list in program.
+ *@head: Address of the first node in a list.
+ *@str: Address of the string to insert into the new node created.
+ *Return: Address of the new node.
  **/
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t* add_node_end(list_t **head, const char *str)
 {
 	list_t *cond, *cond2;
 	unsigned int length = 0;
 
 	if (str == NULL)
-	return (NULL);
+		return (NULL);
 
 	cond = malloc(sizeof(list_t));
 	if (cond == NULL)
-	return (NULL);
+		return (NULL);
 
 	cond->str = strdup(str);
 	if (cond->str == NULL)
@@ -25,21 +25,21 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(cond);
 		return (NULL);
 	}
+
 	while (str[length])
 		length++;
-		cond->len = length;
-		cond->forward = NULL;
+	cond->len = length;
+	cond->forward = NULL;
 
-		if (*head == NULL)
-		{
-			*head = cond;
-			return (cond);
-		}
+	if (*head == NULL)
+	{
+		*head = cond;
+		return (cond);
+	}
 
-		cond2 = *head;
+	cond2 = *head;
 	while (cond2->forward)
 		cond2 = cond2->forward;
-		cond2->forward = cond;
-		return (cond);
+	cond2->forward = cond;
+	return (cond);
 }
-
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,13 +2,13 @@
 #include "lists.h"
 
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ *free_list - frees a linked list
+ *@head: list_t list to be freed
  */
 
 void free_list(list_t *head)
 {
-	list_t *cond;
+	list_t * cond;
 
 	while (head)
 	{
