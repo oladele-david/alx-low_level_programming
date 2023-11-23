43be095f8a8d9808193fbb3d98782b8a96e1b1c1
oladele-david <tdboy59@gmail.com>
2023-11-22 21:09:51 -0800

    tasks

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *find_listint_loop - finds the loop in a linked list
+ *@head: pointer to the head of the listint_t linked list
+ *
+ *Return: The address of the node where the loop starts, or NULL if there is no loop
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
