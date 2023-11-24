29fefc66384b0cbe597248d2cbf8d462eccfd589
oladele-david <tdboy59@gmail.com>
2023-11-24 05:19:21 -0800

    Task 4fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,9 +12,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 	{
-		return -1; /*Invalid index */
+		return (-1); /*Invalid index */
 	}
 
-	*n = *n &~(1UL << index);
-	return 1;
+	*n = *n & ~(1UL << index);
+	return (1);
 }
